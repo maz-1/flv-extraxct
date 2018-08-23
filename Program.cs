a960f0805a483c45ef47537f97c382c5a42de9df
maz-1 <ohmygod19993@gmail.com>
2018-08-23 12:07:06 +0000

    improve linux support

--- Program.cs
+++ Program.cs
@@ -2,6 +2,8 @@ using System;
 using System.Drawing;
 using System.Windows.Forms;
 using System.IO;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
 
 namespace JDP
 {
@@ -21,30 +23,57 @@ namespace JDP
         public static void SetFontAndScaling(Form form)
         {
             form.SuspendLayout();
-            string firstLine;
-            string DefaultFontName = "Tahoma";
-            string FontConf = Application.StartupPath + System.IO.Path.DirectorySeparatorChar + "font.txt";
-            if (!File.Exists(FontConf))
-            {
-                using (StreamWriter writer = new StreamWriter(FontConf))
-                {
-                    writer.Write(DefaultFontName);
-                }
+            int platformId = (int) Environment.OSVersion.Platform;
+            //Detect Unix
+            if ((platformId == 4) || (platformId == 128)) {
+                Regex UnixDefaultFontRegexp = new Regex(@"^'(.*) \d*'$", RegexOptions.Compiled);
+                string DefaultFont = UnixDefaultFontRegexp.Match(ExecuteBashCommand("gsettings get org.gnome.desktop.interface font-name")).Groups[1].ToString();
+                //MessageBox.Show(DefaultFont);
+                if (DefaultFont != "")
+                    form.Font = new Font(DefaultFont, 8.25f);
+                else
+                    form.Font = new Font("Tahoma", 8.25f);
             }
-            if (File.Exists(FontConf))
+            /*
+            //macos tobedone
+            else if (platformId == 6)
             {
-              using (StreamReader reader = new StreamReader(FontConf))
-              {
-                  firstLine = reader.ReadLine() ?? "";
-              }
-              if (firstLine!="")
-                  DefaultFontName = firstLine;
+                
             }
-            form.Font = new Font(DefaultFontName, 8.25f);
-            if (form.Font.Name != DefaultFontName) form.Font = new Font("Arial", 8.25f);
+            */
+            else
+            {
+                form.Font = new Font("Tahoma", 8.25f);
+                if (form.Font.Name != "Tahoma") form.Font = new Font("Arial", 8.25f);
+            }
             form.AutoScaleMode = AutoScaleMode.Font;
             form.AutoScaleDimensions = new SizeF(6f, 13f);
             form.ResumeLayout(false);
+        }
+        
+        
+        private static string ExecuteBashCommand(string command)
+        {
+            // according to: https://stackoverflow.com/a/15262019/637142
+            // thans to this we will pass everything as one command
+            command = command.Replace("\"","\"\"");
+            
+            var proc = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "/bin/bash",
+                    Arguments = "-c \""+ command + "\"",
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    CreateNoWindow = true
+                }
+            };
+        
+            proc.Start();
+            proc.WaitForExit();
+
+            return proc.StandardOutput.ReadToEnd();
         }
     }
 }
